4213ae03c5d1f8cc6ec1e97c3db76e0445b4fc9f
D-Mariann <119792440+D-Mariann@users.noreply.github.com>
2023-04-30 15:54:11 +0300

    Add files via upload

---
+++ tests/first_test.py
@@ -0,0 +1,38 @@
+from app.calculator import Calculator
+import pytest
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_adding_success(self):
+        assert self.calc.adding(self, 1, 1) == 2
+
+    def test_adding_unsuccess(self):
+        assert self.calc.adding(self, 1, 1) == 3
+
+    def test_multiply_success(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_multiply_unsuccess(self):
+        assert self.calc.multiply(self, 2, 0) == 2
+
+    def test_division_success(self):
+        assert self.calc.division(self, 8, 2) == 4
+
+    def test_division_unsuccess(self):
+        assert self.calc.division(self, 8, 2) == 2
+
+    def test_zero_division(self):
+        with pytest.raises(ZeroDivisionError):
+            self.calc.division(self,1,0)
+
+    def test_subtraction_success(self):
+        assert self.calc.subtraction(self, 8, 2) == 6
+
+    def test_subtraction_unsuccess(self):
+        assert self.calc.subtraction(self, 8, 2) == 2
+
+    def teardown(self):
+        print('Выполнение метода Teardown')
